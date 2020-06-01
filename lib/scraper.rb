7e83800cb0c3d27a09bd75be4b5000094abd8436
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-01 20:06:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css(".headline-26OIBN")
+puts doc.css(".headline-26OIBN").text
 
 # puts doc
